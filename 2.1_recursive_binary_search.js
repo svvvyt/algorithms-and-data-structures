1e6285cdd720195c831aa2633d537a35b178e506
svvyt <lolovishka@mail.ru>
2022-06-24 23:43:46 +0400

    Added recursive binary search algorithm

---
+++ 2.1_recursive_binary_search.js
@@ -0,0 +1,13 @@
+// Рекурсивный алгоритм бинарного поиска
+
+const arraySample = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+let iterations = 0;
+
+function recursiveBinarySearch() {}
+
+console.log(recursiveBinarySearch(arraySample, 11));
+console.log(`iterations = ${iterations}`);
+
+// Сложность: O(log2n)
+
+// только для отсортированных массивов
