c94951eef972de18e290b28e0064de2c354695b1
svvyt <lolovishka@mail.ru>
2022-06-24 23:52:22 +0400

    Added linked list data structure

---
+++ 10_linked_list.js
@@ -0,0 +1,62 @@
+// Связный список:
+
+// - каждый отдельный элемент занимает отдельное место в памяти
+// - каждый предыдущий элемент хранит ссылку на следующий
+
+// +: можно мгновенно добавлять элемент в начало или конец
+// -: чтобы получить какой-то элемент, нужно с самого начала итерироваться и сравнивать
+
+// O(1) - вставка/удаление в начало/конец, если знаем место
+// O(n) - вставка в произвольное место, если не знаем место
+// O(n) - поиск по списку
+
+class LinkedList {
+  constructor() {
+    this.size = 0;
+    this.root = null;
+  }
+  add(value) {
+    if (this.size === 0) {
+      this.root = new Node(value);
+      this.size += 1;
+      return true;
+    }
+    let node = this.root;
+    while (node.next) {
+      node = node.next;
+    }
+    let newNode = new Node(value);
+    node.next = newNode;
+    this.size += 1;
+  }
+
+  getSize() {
+    return this.size;
+  }
+
+  print() {
+    let result = [];
+    let node = this.root;
+    while (node) {
+      result.push(node.value);
+      node = node.next;
+    }
+    console.log(result);
+  }
+}
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+const list = new LinkedList();
+list.add(5);
+list.add(3);
+list.add(2);
+list.add(5);
+list.add(7);
+
+list.print();
