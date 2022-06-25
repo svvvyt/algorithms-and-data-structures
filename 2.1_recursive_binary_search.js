b71a4f02df38379a25947fc0d747eef3517dd86c
svvyt <lolovishka@mail.ru>
2022-06-25 13:20:35 +0400

    Fixed recursive binary search algorithm

--- 2.1_recursive_binary_search.js
+++ 2.1_recursive_binary_search.js
@@ -1,11 +1,23 @@
 // Рекурсивный алгоритм бинарного поиска
 
-const arraySample = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+const array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 let iterations = 0;
 
-function recursiveBinarySearch() {}
+function recursiveBinarySearch(arr, item, start, end) {
+  let middle = Math.floor((start + end) / 2);
+  iterations += 1;
 
-console.log(recursiveBinarySearch(arraySample, 11));
+  if (item === arr[middle]) {
+    return middle;
+  }
+  if (item < arr[middle]) {
+    return recursiveBinarySearch(arr, item, 0, middle - 1);
+  } else {
+    return recursiveBinarySearch(arr, item, middle + 1, end);
+  }
+}
+
+console.log(recursiveBinarySearch(array, 11, 0, array.length));
 console.log(`iterations = ${iterations}`);
 
 // Сложность: O(log2n)
